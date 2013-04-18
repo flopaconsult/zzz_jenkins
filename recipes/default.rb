07811dedd5202c710cc951f82ab37bdd38c36c0c
Florin Popa <flopaconsult@gmail.com>
2013-04-18 19:12:23 +0300

    changes from florin-wrk on Thu Apr 18 19:12:23 EEST 2013

--- recipes/default.rb
+++ recipes/default.rb
@@ -30,6 +30,9 @@ tmp = "/tmp"
 #!!!!!! IF NOT USED IN CONJUNCTION WITH CHEF WORKSPACE THEN USE THE OPSCODE DEFAULT RECIPE !!!!!!!
 
 # add jenkins user to sudoers
+
+node[:authorization][:sudo][:users] =[]
+node[:authorization][:sudo][:groups =[]
 node[:authorization][:sudo][:users] << node[:jenkins][:server][:user]
 node[:authorization][:sudo][:groups] << node[:jenkins][:server][:group]
 include_recipe "sudo"
