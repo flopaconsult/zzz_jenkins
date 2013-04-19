69abb2ea7713cfb4d13d6d0b23e273df54179f2c
Florin Popa <flopaconsult@gmail.com>
2013-04-19 13:28:43 +0300

    changes from florin-wrk on Fri Apr 19 13:28:43 EEST 2013

--- metadata.rb
+++ metadata.rb
@@ -6,5 +6,5 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.6.3"
 
 depends 'runit','~> 1.0.0'
-%w(java apt apache2 git sudo iptables jenkins).each { |cb| depends cb }
+%w(java apt apache2 zzz_sudo git iptables jenkins).each { |cb| depends cb }
 %w(yum).each { |cb| recommends cb }
