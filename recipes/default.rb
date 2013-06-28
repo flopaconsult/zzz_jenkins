14965a7897f85cf3a97c817274cda67b105041c1
flopaconsult <flopaconsult@gmail.com>
2013-06-28 22:57:58 +0300

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -20,7 +20,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
+# test
 
 Chef::Log.info("!!!!! #{node[:tags]}")
 pkey = "#{node[:jenkins][:server][:home]}/.ssh/id_rsa"
